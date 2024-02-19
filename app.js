4b50995bd47b3e61118f4322ca300346b741aa2f
hirokiwa <hirokifsys@gmail.com>
2024-02-19 23:56:33 +0900

    add endpoint

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const REDIRECT_URL = process.env.REDIRECT_URL || 'https://example.com';
+console.log(process.env.REDIRECT_URL_1)
+
+const DEFAULT_URL = 'https://example.com';
+const REDIRECT_URL_1 = process.env.REDIRECT_URL_1 ?? DEFAULT_URL;
+const REDIRECT_URL_2 = process.env.REDIRECT_URL_2 ?? DEFAULT_URL;
+const REDIRECT_URL_3 = process.env.REDIRECT_URL_3 ?? DEFAULT_URL;
 
 app.get('/', (_, res) => {
   res.redirect(301, '/1');
 });
 
 app.get('/1', (_, res) => {
-  res.redirect(301, REDIRECT_URL);
+  res.redirect(301, REDIRECT_URL_1);
+});
+
+app.get('/2', (_, res) => {
+  res.redirect(301, REDIRECT_URL_2);
+});
+
+app.get('/3', (_, res) => {
+  res.redirect(301, REDIRECT_URL_3);
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
